426b8ef890e011bfabe44c5cde4eb852284c9fac
Simon Papanicoloau <spapanicolaou@travelrepublic.co.uk>
2020-02-07 17:18:18 +0000

    added sticky menu for kitchensink page with svg highlight and stroke effect

--- src/js/functions.js
+++ src/js/functions.js
@@ -6,16 +6,13 @@ function measureEls( arr ) {
 	arrLen = arr.length;
 	for( let i = arrLen - 1; i >= 0; i-- ) {
 		let currEl = $( arr[ i ] );
-		let currElContent = currEl.find( '[ data-reveal-content ]' );
-		let thisHeight = currEl.innerHeight();
 
 		currEl
-			.attr( 'data-open-height', thisHeight )
+			.attr( 'data-open-height', currEl.innerHeight() )
 			.css( {
-				'height':  thisHeight,
+				'height':  0,
 			} )
-			.addClass( 'scaledY-zero transitioner' );
-		currElContent.addClass( 'scaledY-dble' );
+			.addClass( 'transitioner' );
 	}
 }
 
@@ -27,22 +24,23 @@ measureEls( $revealEls );
 $( '[ data-reveal-trigger ]' ).click( function( e ){
 	$this = $( this );
 	$linkedEl = $( `[ data-reveal-target="${$this.attr( 'data-reveal-trigger' )}" ]` );
-	let openHeight = $linkedEl.attr( 'data-open-height' )+'px';
-	$linkedElContent = $linkedEl.find( '[ data-reveal-content ]' );
+	let thisHeight = $linkedEl.attr( 'data-open-height' )+'px';
 
 	if ( $linkedEl.hasClass( 'is-active' ) ) {
-		$linkedEl.removeClass( 'is-active scaledY-init' ).addClass( 'scaledY-zero' );
-		$linkedElContent.removeClass( 'scaledY-init' ).addClass( 'scaledY-dble' );
+		$linkedEl
+			.css( {
+				'height':  0,
+			} )
+			.removeClass( 'is-active' );
 		$this.removeClass( 'is-active' );
 	} else {
 
 		// $linkedEl.removeClass( 'transitioner' );
 		$linkedEl
-			.removeClass( 'scaledY-zero' )
-			.addClass( 'is-active scaledY-init transitioner' );
-		$linkedElContent
-			.removeClass( 'scaledY-dble' )
-			.addClass( 'transitioner scaledY-init' );
+			.css( {
+				'height':  thisHeight,
+			} )
+			.addClass( 'is-active' );
 
 		$this.addClass( 'is-active' );
 	}
@@ -50,4 +48,133 @@ $( '[ data-reveal-trigger ]' ).click( function( e ){
 } );
 
 });
+
+window.onload = function() {
+
+	var toc = document.querySelector( '.toc' );
+	var tocPath = document.querySelector( '.toc-marker path' );
+	var tocItems;
+
+	// Factor of screen size that the element must cross
+	// before it's considered visible
+	var TOP_MARGIN = 0.00,
+		BOTTOM_MARGIN = 0.0;
+
+	var pathLength;
+
+	var lastPathStart,
+		lastPathEnd;
+
+	window.addEventListener( 'resize', drawPath, false );
+	window.addEventListener( 'scroll', sync, false );
+
+	drawPath();
+
+	function drawPath() {
+
+		tocItems = [].slice.call( toc.querySelectorAll( 'li' ) );
+
+		// Cache element references and measurements
+		tocItems = tocItems.map( function( item ) {
+			var anchor = item.querySelector( 'a' );
+			var target = document.getElementById( anchor.getAttribute( 'href' ).slice( 1 ) );
+
+			return {
+				listItem: item,
+				anchor: anchor,
+				target: target
+			};
+		} );
+
+		// Remove missing targets
+		tocItems = tocItems.filter( function( item ) {
+			return !!item.target;
+		} );
+
+		var path = [];
+		var pathIndent;
+
+		tocItems.forEach( function( item, i ) {
+
+			var x = item.anchor.offsetLeft - 5,
+				y = item.anchor.offsetTop,
+				height = item.anchor.offsetHeight;
+
+			if( i === 0 ) {
+				path.push( 'M', x, y, 'L', x, y + height );
+				item.pathStart = 0;
+			}
+			else {
+				// Draw an additional line when there's a change in
+				// indent levels
+				if( pathIndent !== x ) path.push( 'L', pathIndent, y );
+
+				path.push( 'L', x, y );
+
+				// Set the current path so that we can measure it
+				tocPath.setAttribute( 'd', path.join( ' ' ) );
+				item.pathStart = tocPath.getTotalLength() || 0;
+
+				path.push( 'L', x, y + height );
+			}
+
+			pathIndent = x;
+
+			tocPath.setAttribute( 'd', path.join( ' ' ) );
+			item.pathEnd = tocPath.getTotalLength();
+
+		} );
+
+		pathLength = tocPath.getTotalLength();
+
+		sync();
+
+	}
+
+	function sync() {
+
+		var windowHeight = window.innerHeight;
+
+		var pathStart = pathLength,
+			pathEnd = 0;
+
+		var visibleItems = 0;
+
+		tocItems.forEach( function( item ) {
+
+			var targetBounds = item.target.getBoundingClientRect();
+
+			if( targetBounds.bottom > windowHeight * TOP_MARGIN && targetBounds.top < windowHeight * ( 1 - BOTTOM_MARGIN ) ) {
+				pathStart = Math.min( item.pathStart, pathStart );
+				pathEnd = Math.max( item.pathEnd, pathEnd );
+
+				visibleItems += 1;
+
+				item.listItem.classList.add( 'visible' );
+			}
+			else {
+				item.listItem.classList.remove( 'visible' );
+			}
+
+		} );
+
+		// Specify the visible path or hide the path altogether
+		// if there are no visible items
+		if( visibleItems > 0 && pathStart < pathEnd ) {
+			if( pathStart !== lastPathStart || pathEnd !== lastPathEnd ) {
+				tocPath.setAttribute( 'stroke-dashoffset', '1' );
+				tocPath.setAttribute( 'stroke-dasharray', '1, '+ pathStart +', '+ ( pathEnd - pathStart ) +', ' + pathLength );
+				tocPath.setAttribute( 'opacity', 1 );
+			}
+		}
+		else {
+			tocPath.setAttribute( 'opacity', 0 );
+		}
+
+		lastPathStart = pathStart;
+		lastPathEnd = pathEnd;
+
+	}
+
+};
 
