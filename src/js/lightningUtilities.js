57b2d2f5a31c1c86889fed39496da4b036806646
Simon Papanicoloau <spapanicolaou@travelrepublic.co.uk>
2020-03-16 16:51:06 +0000

    starting on the lightning constructor function

--- src/js/lightningUtilities.js
+++ src/js/lightningUtilities.js
@@ -11,6 +11,18 @@ let ligntningMgr = {
 		intervalCurrent: 0
 	},
 
+	creationConfig: {
+		branches: {
+			spawnRate: {
+				min: 0,
+				max: 0
+			},
+			min: 0,
+			max: 0
+		},
+
+	},
+
 	renderConfig: {},
 
 	clock: {
@@ -31,6 +43,7 @@ let ligntningMgr = {
 			this.globalConfig,intervalMax
 			);
 	},
+
 	startGlobalClock: function() {
 		this.clock.global.isRunning = true;
 	},
@@ -59,31 +72,88 @@ let ligntningMgr = {
 		}
 	},
 
-	createMainPath: function() {
-
-	},
-
-	createChildPath: function() {
-
-	},
-
 	createPath: function( options, dest ) {
 		let lMgr = this;
 		let opts = options;
 
-		lPath = {
-			isChild: opts.isChild ? opts.isChild : false,
-			renderOffset: opts.renderOffset ? 0 : opts.renderOffset,
-			path: []
-		}
+		let temp = [];
+		temp.push( { x: opts.startX, y: opts.startY } );
+		temp.push( { x: opts.endX, y: opts.endY } );
+		plotPoints( temp, opts.subdivisions );
+
+		let lPath = {
+			isChild: opts.isChild || false,
+			branchDepth: opts.branchDepth || 0,
+			renderOffset: opts.renderOffset || 0,
+			path: temp
+		};
 
 		dest.push( lPath );
 
 	},
 
-	createLightning: function( originX, originY, targetX, targetY ) {
+	createBranchPoints: function( pathArray, branchCount ) {
+		let pLen = pathArray.length;
+		let temp = [];
+		for( let i = branchCount; i > 0; i-- ) {
+			let branchP = pathArray[ mathutils.random( 0, pLen ) ];
 
+			temp.push( mathutils.random( 0, pLen ) );
+		};
+		return temp;
 	},
+
+	createLightning: function( options ) {
+
+		let lMgr = this;
+		let opts = options;
+		let creationConfig = this.creationConfig;
+		let branchCfg = creationConfig.branches;
+		let branchCount = mathUtils.randomInteger(
+			branchCfg.min, branchCfg.max
+		);
+		let tempPaths = [];
+		let branchCurr = 0;
+
+		// 1. create intial path
+			// 2. increment branch depth level
+				// 3. for each path at branch depth level
+					// 3.a for each branch point
+						// 3.b create path
+						// goto 3
+
+
+		for(let i = 0, i < branchCount; i++){
+			if ( i === 0 ) {
+				this.createPath(
+					{
+						isChild: false,
+						branchDepth: 0,
+						renderOffset: 0,
+						startX: opts.startX,
+						startY: opts.startY,
+						endX: opts.endX,
+						endY: opts.endY,
+						subdivisions: opts.subdivisions
+					},
+					tempPaths
+				);
+			} else {
+
+			}
+
+		}
+
+		let lInstance = {
+			isActive: true,
+			status: {
+				renderPhase: 0
+			},
+			paths: tempPaths
+		}
+
+		this.members.push( lInstance );
+	}
 }
 
 module.exports = ligntningMgr;
